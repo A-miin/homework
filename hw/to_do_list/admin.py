925b05ae4afb1bd0dc963542cd5175c58215c31d
A-miin <zulaykaisaeva@gmail.com>
2021-02-25 16:43:48 +0600

    register model in admin

--- hw/to_do_list/admin.py
+++ hw/to_do_list/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Card
 
 # Register your models here.
+
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'date']
+    list_filter = [ 'description', 'status', 'date']
+    search_fields = [ 'description', 'status', 'date']
+    fields = [ 'description', 'status', 'date']
+    readonly_fields = []
+
+
+admin.site.register(Card, CardAdmin)
