919e6e771044e77c3bed7ec6e25c4dfdcf5d36b8
A-miin <zulaykaisaeva@gmail.com>
2021-02-25 17:02:47 +0600

    add styles to index

--- hw/to_do_list/views.py
+++ hw/to_do_list/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from .models import Ca
+from .models import Card
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    cards = Card.objects.all()
+    context = {
+        'cards':cards
+    }
+    return render(request, 'index.html', context)
