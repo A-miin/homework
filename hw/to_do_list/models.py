eaeefcc5c5d3b2e4315469f6a2b968bbf26acb1d
A-miin <zulaykaisaeva@gmail.com>
2021-02-25 16:05:20 +0600

    Add models

--- hw/to_do_list/models.py
+++ hw/to_do_list/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
 # Create your models here.
+class Card(models.Model):
+    description = models.CharField(max_length=255, null=False, blank=False)
+    status = models.CharField(max_length=32, null=False, blank=False, choices=status_choices, default='new')
+    date = models.DateField(null=True)
+
+    class Meta:
+        db_table = 'cards'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return f'{self.id}. {self.description}: {self.status}'
