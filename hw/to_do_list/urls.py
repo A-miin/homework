4f6598cb292cbc293777b3ce4b6df3f7a85faa6b
A-miin <zulaykaisaeva@gmail.com>
2021-03-03 17:16:34 +0600

    add updating

--- hw/to_do_list/urls.py
+++ hw/to_do_list/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from .views import index, card_create, delete_card, card_view
+from .views import (
+                    index,
+                    card_create,
+                    card_delete,
+                    card_view,
+                    card_update,
+                )
 
 urlpatterns = [
     path('', index,name='list'),  # URL для отображения списка статей
     path('new/', card_create, name='new'),
-    path('delete/<int:pk>', delete_card, name='delete' ),
-    path('card/<int:pk>/', card_view, name='card-view')
+    path('delete/<int:pk>', card_delete, name='card-delete' ),
+    path('card/<int:pk>/', card_view, name='card-view'),
+    path('card/<int:pk>/edit', card_update, name='card-update'),
 ]
 
--- hw/to_do_list/views.py
+++ hw/to_do_list/views.py
@@ -40,6 +40,29 @@ def card_create(request):
         )
         return redirect('card-view', pk=card.id)
 
-def delete_card(request, pk):
-    Card.objects.filter(id=pk).delete()
+def card_delete(request, pk):
+    card = Card.objects.filter(id=pk)
+    if request.method.GET:
+        return render(request,'card_delete.html',card)
+
     return redirect('list')
+
+def card_update(request, pk):
+    card = get_object_or_404(Card, id=pk)
+    if request.method == "GET":  # Если метод запроса GET - будет отображена форма создания статьи
+        return render(request, 'card_update.html', {'card':card})
+    elif request.method == "POST":  # Если метод запроса POST - будет отображён шаблон просмотра деталей статьи
+        name = request.POST.get("name")
+        status = request.POST.get("status")
+        date = request.POST.get("date")
+        description = request.POST.get('description')
+        if date=="":
+            date=None
+        if description=="":
+            description=None
+        card.name = name
+        card.status = status
+        card.date = date
+        card.description = description
+        card.save()
+        return redirect('card-view', pk=card.id)
