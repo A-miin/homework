de758d4cec8f24683e3f3863c349323188c6f016
A-miin <zulaykaisaeva@gmail.com>
2021-03-04 17:43:45 +0600

    Add modelForm to update

--- hw/to_do_list/views.py
+++ hw/to_do_list/views.py
@@ -34,23 +34,6 @@ def card_create(request):
             return redirect('card-view',pk = card.id)  # Перенаправляем клиента на страницуу детального просмотра статьи
         return render(request, 'create_card.html',context={'form': form})  # если форма не валидна - отобразим форму с ошибками
 
-
-"""
-    if request.method == "GET":  # Если метод запроса GET - будет отображена форма создания статьи
-s
-        return render(request, 'article_create.html', context={'form': form})
-    elif request.method == "POST":  # Если метод запроса POST - создаём статью и редиректим клиента
-        form = ArticleForm(data=request.POST)  # Создадим объект формы, в него передадим данные из формы, которые пришли от клиента
-        if form.is_valid():  # если форма валидна - создаётся статья и клиент редиректится
-            article = Article.objects.create(
-                title=form.cleaned_data.get('title'),
-                content=form.cleaned_data.get('content'),
-                author=form.cleaned_data.get('author')
-            )
-            return redirect('article-view', pk=article.id)  # Перенаправляем клиента на страницуу детального просмотра статьи
-        return render(request, 'article_create.html', context={'form': form})  # если форма не валидна - отобразим форму с ошибками
-
-"""
 def card_delete(request, pk):
     card = get_object_or_404(Card, id=pk)
     if request.method=='GET':
@@ -65,19 +48,21 @@ def card_delete(request, pk):
 def card_update(request, pk):
     card = get_object_or_404(Card, id=pk)
     if request.method == "GET":  # Если метод запроса GET - будет отображена форма создания статьи
-        return render(request, 'card_update.html', {'card':card})
+        form = CardForm(
+            initial={  # создадим форму со стартоввыми данными полей, соответствующими данным полей статьи
+                'name': card.name,
+                'status':card.status,
+                'date':card.date,
+                'description': card.description
+            })
+        return render(request, 'card_update.html', {'card':card,'form':form})
     elif request.method == "POST":  # Если метод запроса POST - будет отображён шаблон просмотра деталей статьи
-        name = request.POST.get("name")
-        status = request.POST.get("status")
-        date = request.POST.get("date")
-        description = request.POST.get('description')
-        if date=="":
-            date=None
-        if description=="":
-            description=None
-        card.name = name
-        card.status = status
-        card.date = date
-        card.description = description
-        card.save()
-        return redirect('card-view', pk=card.id)
+        form = CardForm(data=request.POST)
+        if form.is_valid():
+            card.name = request.POST.get("name")
+            card.status = request.POST.get("status")
+            card.date = request.POST.get("date")
+            card.description = request.POST.get('description')
+            card.save()
+            return redirect('card-view', pk=card.id)
+        return render(request, 'card_update.html', {'card':card, 'form': form})
